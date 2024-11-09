595567b1f255102c9bad5bfab227c6b23b6f2a36
Viviane da Costa <vivianecf22@gmail.com>
2024-11-09 14:12:13 -0300

    Update index.js
    Teste

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 app.use('/api/users', logoutRoute);
 
-app.get('/', (req, res) => {
+app.get('/Hello', (req, res) => {
   res.send('Hello World!');
 });
 
